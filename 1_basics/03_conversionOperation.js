c69a10600840deeadb119e199a4f3935f52be013
Codebypallavi.in <86648531+pallavigupta2021@users.noreply.github.com>
2025-07-24 21:44:14 +0000

    Operations

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -1,3 +1,6 @@
+
+ // ******************************* converison of data types ****************************************** //
+
 // let score = "11888"
 // let score = "ll888"
 // let score = undefined
@@ -14,6 +17,9 @@ let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 
 
+/////////////////////////////////////////////////////////////////////////
+
+
 //  "33" => 33
 // "33abc" => NaN
 // true => 1; false =>0 
@@ -23,16 +29,64 @@ let valueInNumber = Number(score)
 let isLoggedIn = "pallavi"
 
 let booleanIsLoggedIn  = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false 
 // "" => false 
 // "pallavi" => true
 
+
+//////////////////////////////////////////////////////////////////////////////
+
 let somenumber = 44
 
-console.log(typeof somenumber);
+//console.log(typeof somenumber);
 
 let stringNumber = String(somenumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+///console.log(typeof stringNumber);
+
+
+
+// **************************************************** Operation ********************************************* //
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2-2);
+//console.log(2**2);
+//console.log(2+3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1  = "pallavi"
+let str2 = " gupta"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+// console.log((3+4) * 4 % 3);
+
+console.log(+true)
+console.log(+"")
+
+let num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+ 
+console.log(gameCounter);
+
+
+// study to document https://tc39.es/ecma262/#sec-type-conversion
+// prefix / post fix https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
