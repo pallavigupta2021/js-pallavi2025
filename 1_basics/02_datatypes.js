6bd0dd1863e2c070e770c263e8970f0837408128
Codebypallavi.in <86648531+pallavigupta2021@users.noreply.github.com>
2025-07-24 20:32:46 +0000

    datatypes day1

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// alert (3+3)  //we are using node.js , not browser 
+
+console.log(3
+    +
+    3); // this is not correct way to code define code should be high and readbility 
+
+console.log("pallavi");
+
+
+let name = "pallavi";
+let age = 18 
+let isLoggedIn  = false
+let state;
+
+// number => 2 to power 53 
+// bigint  
+// string => ""
+// boolean => true/false
+// null  => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "pallavi"); // string 
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
