d42a17048bc086bf06dae91946abfc0044b32c99
Codebypallavi.in <86648531+pallavigupta2021@users.noreply.github.com>
2025-07-24 21:13:50 +0000

    conversion of data tyoes day1

---
+++ 1_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "11888"
+// let score = "ll888"
+// let score = undefined
+//let score = null
+ let score = true
+
+
+//console.log(typeof score );
+//console.log(typeof (score) )
+
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+//  "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0 
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "pallavi"
+
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "pallavi" => true
+
+let somenumber = 44
+
+console.log(typeof somenumber);
+
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
